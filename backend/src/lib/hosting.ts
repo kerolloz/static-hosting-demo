0cf384af7467f534f64254fd8d862127c6e3352e
Kerollos Magdy <kerolloz@yahoo.com>
2024-10-13 19:23:57 +0300

    fix hosting dir

--- backend/src/lib/hosting.ts
+++ backend/src/lib/hosting.ts
@@ -1,4 +1,4 @@
-export const STATIC_HOSTING_DIR = '~/var/www/static-hosting' as const;
+export const STATIC_HOSTING_DIR = '/srv/static-hosting' as const;
 export const STATIC_HOSTING_DOMAIN = 'lvh.me' as const;
 
 export const getStaticHostingUrl = (subdomain: string) =>
